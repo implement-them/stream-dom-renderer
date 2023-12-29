327746166fa26e767deb2a0ab5fbaf9df8c8fbf7
Liuying <liuyingyyx@dingtalk.com>
2023-12-29 22:29:31 +0800

    chore: update readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Stream Dom Renderer
+
+Read stream input and render them to web page.
+
+## 
+
+## Change log:
+
+## Milestones & TODO list:
+
+- [] execute basic dom operators
+- [] support 'internal' stream input
+- [] support official markdown stream as input (exclude code block)
+- [] support one source code highlighting
+- [] support custom generated rules
+
---
+++ scripts/postbuild.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.copyFileSync(path.resolve(__dirname, '../README.md'), path.resolve(__dirname, '../dist/README.md'));
+
+const pkgs = fs.readFileSync(path.resolve(__dirname, '../package.json'), { encoding: 'utf-8' });
+const pkg = JSON.parse(pkgs);
+
+const list = [
+  'name',
+  'version',
+  'author',
+  'main',
+  'module',
+  'peerDependencies'
+];
+
+const newPkg = {};
+
+list.forEach((k) => {
+  if (pkg[k]) {
+    newPkg[k] = pkg[k];
+  }
+});
+
+fs.writeFileSync(
+  path.resolve(__dirname, '../dist/package.json'),
+  JSON.stringify(newPkg),
+)
+
