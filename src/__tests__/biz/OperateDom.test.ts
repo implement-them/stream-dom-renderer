032f46a73cec163e92c47b8ddef57ee9c51db835
Liuying <liuyingyyx@dingtalk.com>
2023-12-30 21:29:04 +0800

    test: update test case of dom command

--- src/__tests__/biz/OperateDom.test.ts
+++ src/__tests__/biz/OperateDom.test.ts
@@ -1,10 +1,14 @@
 import '../init';
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, beforeEach } from 'vitest'
 import { createStreamRenderer } from '../../libs';
 import { DomCommand } from '../../libs/Commands/DomCommand';
+import { appendTextCases } from '../TestCases/DomManager/appendText';
 
-describe('DOM Command 操作节点', () => {
-  test('写入字符串',  async () => {
+describe('DOM Command', () => {
+  beforeEach(async () => {
+    document.body.innerHTML = '';
+  });
+  test('[DOM Command]-Free',  async () => {
     const renderer = createStreamRenderer().use({
       commands: [
         new DomCommand(),
@@ -20,5 +24,15 @@ describe('DOM Command 操作节点', () => {
     expect(renderer.domManager.currentNode.textContent).toBe('hehehehe1');
     expect(renderer.domManager.currentNode).toEqual(document.body.firstChild);
   });
-});
 
+  test.each(appendTextCases)('[DOM Command]- dom.append_text $input', async ({ input, innerHTML, textContent }) => {
+    const renderer = createStreamRenderer().use({
+      commands: [
+        new DomCommand(),
+      ],
+    });
+    await renderer.execute('dom.append_text', { text: input });
+    expect(renderer.domManager.currentNode.innerHTML).toBe(innerHTML);
+    expect(renderer.domManager.currentNode.textContent).toBe(textContent);
+  });
+});
