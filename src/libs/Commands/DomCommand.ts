f14b307380a12b4af31af6fd4a6195828768ea4d
Liuying <liuyingyyx@dingtalk.com>
2023-12-31 15:05:11 +0800

    feat: define common payload type

--- src/libs/Commands/DomCommand.ts
+++ src/libs/Commands/DomCommand.ts
@@ -1,15 +1,30 @@
+import type { StreamDomRenderer } from '../StreamDomRenderer';
 import { AbstractCommand } from './Abstract';
 
+export interface IDomCommands {
+  'dom.append_child': {
+    node: HTMLElement;
+  };
+  'dom.append_text': {
+    text: string;
+  };
+};
+
+export type IDomCommandPayload<T extends keyof IDomCommands> = IDomCommands[T] & {
+  controller: StreamDomRenderer;
+};
+
 export class DomCommand extends AbstractCommand {
-  public async execute(command: string, payload?: any) {
-    if (!payload.controller) {
+  public async execute<T extends keyof IDomCommands>(command: T, payload?: IDomCommandPayload<T>) {
+    if (!payload || !payload.controller) {
       return;
     }
     switch (command) {
       case 'dom.append_child':
+        payload.controller.domManager.appendChild((payload as IDomCommandPayload<'dom.append_child'>).node);
         break;
       case 'dom.append_text': 
-        payload.controller.domManager.appendText(payload.text);
+        payload.controller.domManager.appendText((payload as IDomCommandPayload<'dom.append_text'>).text);
         break;
       default: break; 
     }
